dff6a9ae704c869d9a467a18006afd63eb670adf
323003814 <323003814@C-JJYTVR1.tdsb.on.ca>
2016-03-02 14:44:52 -0500

    Created Hello World File

---
+++ ICS3U/src/hussain/HelloWorld.java
@@ -0,0 +1,15 @@
+package hussain;
+/*HelloWorld.java
+ * this program prints Hello World
+ * Jared Hussain
+ * March 2, 2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("Hello World");
+	}
+
+}
