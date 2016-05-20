35e66a5ef922b7fda580e928975a6d87c4d8bdd9
323003814 <323003814@C-JJYTVR1.tdsb.on.ca>
2016-05-20 14:41:16 -0400

    using methods, this program outputs the word Mississipi when it is run

---
+++ ICS3U/src/hussain/MississipiProgram.java
@@ -0,0 +1,89 @@
+package hussain;
+
+public class MississipiProgram {
+	/* Jared Hussain
+	 * Mississipi Program
+	 * using methods, this program outputs the word Mississipi when it is run
+	 */
+
+	public static void main(String[] args) {
+		drawM();
+		System.out.println();
+		drawI();
+		System.out.println();
+		drawS();
+		System.out.println();
+		drawS();
+		System.out.println("");
+		drawI();
+		System.out.println("");
+		drawS();
+		System.out.println("");
+		drawS();
+		System.out.println("");
+		drawI();
+		System.out.println("");
+		drawP();
+		System.out.println("");
+		drawI();
+		System.out.println("");
+		
+	}
+
+		public static void drawM(){
+			// TODO Auto-generated method stub
+
+		
+		
+		
+		System.out.println("M       M");
+		System.out.println("MM     MM");
+		System.out.println("M M   M M");
+		System.out.println("M   M   M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		System.out.println("M       M");
+		}
+		
+		public static void drawI(){
+			// TODO Auto-generated method stub
+		
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+		}
+		
+		public static void drawS(){
+			// TODO Auto-generated method stub
+		
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S    S ");
+		System.out.println(" SSSSS ");
+		}
+		
+		public static void drawP(){
+			// TODO Auto-generated method stub
+		
+		System.out.println("PPPPP  ");
+		System.out.println("P    P ");
+		System.out.println("P     P");
+		System.out.println("P    P ");
+		System.out.println("PPPPP  ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		System.out.println("P      ");
+		
+		
+		
+		
+	}
+
+}
