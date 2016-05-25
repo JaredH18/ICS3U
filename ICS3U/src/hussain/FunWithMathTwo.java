36c397a1c4758d0f4b9ef206d6a37f2a55b05eab
323003814 <323003814@C-9S86WR1.tdsb.on.ca>
2016-05-25 14:35:37 -0400

    User gives computer two numbers. computer adds, subtracts, multiplies, and divides them using methods

---
+++ ICS3U/src/hussain/FunWithMathTwo.java
@@ -0,0 +1,64 @@
+package hussain;
+
+import java.util.Scanner;
+
+public class FunWithMathTwo {
+	/* Jared Hussain
+	 * Fun With Math Two Program
+	 * User gives computer two numbers. computer adds, subtracts, multiplies, and divides them using methods
+	 * May 25, 2016
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double num1;
+		double num2;
+		
+		System.out.println("Give me two numbers, I will then add them, subtract them, multipy them, then divide them");
+		System.out.println("what is your first number?");
+		num1 = Double.parseDouble(scan.nextLine());
+		System.out.println("what is your second number?");
+		num2 = Double.parseDouble(scan.nextLine());
+		
+		
+		System.out.println("when you add them, the answerr is " + (addNumbers(num1, num2)));
+		System.out.println("when you subtract them, the answer is " + (subtractNumbers(num1, num2)));
+		System.out.println("when you multiply them, the answer is " + (multiplyNumbers(num1, num2)));
+		System.out.println("when you divide them, the answer is " + (divideNumbers(num1, num2)));
+		
+		
+	}
+	
+	public static double addNumbers( double num1, double num2){
+		
+		double total = num1 + num2;
+		return total;
+		
+		
+	}
+	public static double subtractNumbers( double num1, double num2){
+		
+		double total = num1 - num2;
+		return total;
+		
+		
+	}
+	public static double multiplyNumbers( double num1, double num2){
+		
+		double total = num1 * num2;
+		return total;
+		
+		
+	}
+	public static double divideNumbers( double num1, double num2){
+		
+		double total = num1 / num2;
+		return total;
+		
+		
+	}
+
+}
